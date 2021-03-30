5b084e49398e46500f4162b20d2bd0b6d4fc15c7
Shankar Mahadevan <bala12901@yahoo.com>
2021-03-30 17:57:59 +0530

    Leetcode problem 538

---
+++ Binary Trees/Python/BST/Some Problems/538. Convert BST to Greater Tree.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.sum = 0
+    def convertBST(self, root: TreeNode) -> TreeNode:
+        if root is not None:
+            self.convertBST(root.right)
+            self.sum+=root.val
+            root.val=self.sum
+            self.convertBST(root.left)
+        return root
